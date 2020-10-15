a08b41755c812ad6faedc4ddec45bef346e79336
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-10-14 21:01:31 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Konstantin Novichenko on 10/14/20.
+//  Copyright © 2020 konst_nvc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
